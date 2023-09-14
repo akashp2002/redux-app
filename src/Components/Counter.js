71a9ebdb654d6b9c91ef328446769028fff7fafb
akashp2002 <aachu4010@gmail.com>
2023-09-13 21:50:16 -1200

    react redux

---
+++ src/Components/Counter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Counter.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increment,decrement } from '../Redux/Store/Reducer/Reducer'
+function Counter() {
+    const count=useSelector((state)=>state.counter.value)
+    const dispatch=useDispatch()
+  return (
+    <div className="counter-container">
+      <h2 className="counter-title">React js Redux Example</h2>
+      <p className="counter-value">{count}</p>
+      <div className="counter-buttons">
+        <button className="counter-btn" onClick={()=>dispatch(increment())}>Increment</button>
+        <button className="counter-btn" onClick={()=>dispatch(decrement())} >Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/Redux/Store/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./Reducer/Reducer";
+
+export default configureStore({
+
+    reducer:{
+    counter:counterReducer,
+}
+
+})
